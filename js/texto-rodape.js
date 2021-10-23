20955e2cacbeb2eef4e76d4ed93a598457ab910d
AshileySabah <ashileysabrina2002@gmail.com>
2021-10-22 21:17:26 -0300

    efeito-escrevendo

--- js/texto-rodape.js
+++ js/texto-rodape.js
@@ -1,5 +1,14 @@
-rodape = document.querySelector('[data-rodape]');
+paragrafoCopyright = document.querySelector('[data-rodape-copyright]');
 anoAtual = new Date().getFullYear();
-textoRodape = `ThePokeWorld &copy; ${anoAtual}`;
-rodape.innerHTML = textoRodape;
-console.log(textoRodape)
+textoParagrafoCopyright = `ThePokeWorld &copy; ${anoAtual}`;
+paragrafoCopyright.innerHTML = textoParagrafoCopyright;
+
+paragrafoDesenvolvidoPor = document.querySelector('[data-desenvolvido-por]');
+paragrafoDesenvolvidoPor.innerHTML = '';
+textoDesenvolvidoPor = 'Desenvolvido por Ashiley!';
+arrayTextoDesenvolvidoPor = textoDesenvolvidoPor.split('');
+arrayTextoDesenvolvidoPor.forEach((cadaLetra, posicao) => {
+    setTimeout(() => {
+        paragrafoDesenvolvidoPor.innerHTML += cadaLetra;
+    }, 100*posicao);
+})
