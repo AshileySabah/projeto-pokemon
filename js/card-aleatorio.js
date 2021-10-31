1fc12c36a38ca9ee3baaa03c169c08ecf46b2024
AshileySabah <ashileysabrina2002@gmail.com>
2021-10-31 15:22:28 -0300

    imagem-template-card

--- js/card-aleatorio.js
+++ js/card-aleatorio.js
@@ -6,15 +6,17 @@ const numeroAleatorio = (min, max) => {
 const requisicaoCards = () => {
     for(let i = 1; i <= 2; i++){
         if(i == 1){
-            var imgTeste = document.querySelector('[data-card-img1]');
+            var caixaAlert = document.querySelector('[data-card-img1]');
         }else{
-            var imgTeste = document.querySelector('[data-card-img2]');
+            var caixaAlert = document.querySelector('[data-card-img2]');
         }
 
         const numPokemon = numeroAleatorio(1, 20);
         const url = `https://pokeapi.co/api/v2/pokemon/${numPokemon}`;
 
-        imgTeste.setAttribute('src', `https://cdn.traction.one/pokedex/pokemon/${numPokemon}.png`)
+        caixaAlert.innerHTML = `
+            <img src="https://cdn.traction.one/pokedex/pokemon/${numPokemon}.png" alt="Card de Pokemon" class="img-card">
+        `;
     }
 }
 
